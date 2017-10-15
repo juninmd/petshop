ef7a7366ee996a44c32d40d517bc538f4e6ac885
juninmd <jr_acn@yahoo.com.br>
2017-10-15 14:34:13 -0200

    Ajustando espaços e retirando console.log

--- app/service/encriptar.js
+++ app/service/encriptar.js
@@ -1,30 +1,21 @@
 const bcrypt = require('bcrypt-nodejs')
 
 module.exports = {
-
-    getPassword: ( senha ) => { 
-
-        return new Promise( ( resolve, reject ) => {
-
-            try{
-               let hash = bcrypt.hashSync(senha);
-
-               console.log(hash)
-               
-               resolve({
-                   status: 'success',
-                   message: hash
-               })
-            }
-            catch( err ){
-                console.log(err);
-                reject({
-                    status: 'error',
-                    message: err
+    getPassword: (senha) => {
+        return new Promise((resolve, reject) => {
+            try {
+                let hash = bcrypt.hashSync(senha);
+                return resolve({
+                    status: 'success',
+                    message: hash
                 })
             }
-
-        })        
+            catch (err) {
+                return reject({
+                    status: 'error',
+                    message: err.message
+                })
+            }
+        })
     }
-
 }
